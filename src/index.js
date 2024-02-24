149806b1e67804256570290e301b568aaf937665
Eden-MB <154504714+Eden-MB@users.noreply.github.com>
2024-02-24 18:52:36 +0300

    added typewriter-effect

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-alert {
-    "hello!"
+function formClick(event) {
+  event.preventDefault();
+
+  new Typewriter(".cultural-food", {
+    strings:
+      "Ethiopian food is best described as a culinary adventure bursting with flavour, culture and religious history. It is certainly not for the faint of palates. ",
+    autoStart: true,
+    delay: 2,
+    cursor: "",
+  });
 }
+let form = document.querySelector(".form");
+form.addEventListener("click", formClick);
